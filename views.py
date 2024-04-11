171ba0bb20a6695f2a25af49ed391a1306f0c9b8
Thrisha-krishnamoorthy <139139066+Thrisha-krishnamoorthy@users.noreply.github.com>
2024-04-11 21:40:05 +0530

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-
-
-def home(request):
-    # return HttpResponse("Hello There Polling website")
-
-    return render(request,'pollswebsite/pollswebsite.html')
